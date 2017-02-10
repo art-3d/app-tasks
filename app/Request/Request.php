1d00b2eccbc10eb0823518bf48098c53fd24a0ba
lion.w3x <lion.w3x@gmail.com>
2017-02-11 00:10:59 +0200

    Create Request class

---
+++ app/Request/Request.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Request;
+
+class Request
+{
+    public function isPost()
+    {
+        return $_SERVER['REQUEST_METHOD'] == 'POST';
+    }
+
+    public function post($name)
+    {
+        return isset($_POST[$name]) ? $this->filter($_POST[$name]) : null;
+    }
+
+    public function get($name)
+    {
+        return isset($_GET[$name]) ? $this->filter($_GET[$name]) : null;
+    }
+
+    protected function filter($val)
+    {
+        $val = trim($val);
+        $val = htmlspecialchars($val);
+        return $val;
+    }
+}
