1c28cb3c91607f59cdc533b7ed05847cdfb3b1fc
lion.w3x <lion.w3x@gmail.com>
2017-02-12 17:30:10 +0200

    Update Router class

--- app/Router/Router.php
+++ app/Router/Router.php
@@ -6,31 +6,58 @@ use App\DI\Service;
 
 class Router
 {
-    public static function parseURI($uri)
+    protected $map;
+
+    protected $match;
+
+    public function __construct($map)
     {
-        $params = [];
-        $uri = preg_replace('/\?.+$/', '', $uri);
-        $uriChunks = explode('/', $uri);
-        array_shift($uriChunks);
-        $config = Service::get('app')->config;
+        $this->map = $map;
+    }
 
-        $controller = $config['defaultController'];
+    public function getConfig($uri)
+    {
+        $match = null;
 
-        if ($uri == '/') {
-            $action = $config['defaultAction'];
-        } elseif (count($uriChunks) == 1) {
-            $action = array_shift($uriChunks);
-        } elseif (count($uriChunks) > 1) {
-            $action = array_shift($uriChunks);
-            $params = $uriChunks;
-        } else {
-            throw new \Exception('Route does not found.');
+        foreach ($this->map as $route) {
+            $requirements = $route['requirements'] ? $route['requirements'] : [];
+            $pattern = (count($requirements) > 0) ?
+                $this->preparePattern($route['pattern'], $requirements) :
+                $route['pattern'];
+
+            if ($requirements['_method'] && Service::get('request')->getMethod() != $requirements['_method']) {
+                continue;
+            }
+            if (preg_match($pattern, $uri)) {
+                $match = $route;
+                break;
+            }
+        }
+        if (! $match) {
+            throw new \Exception('404 Page not found');
+        }
+        if ($match['requirements']) {
+            $match['parameters'] = $this->getParameters($match, $uri);
         }
 
-        return [
-            'controller' => $controller,
-            'action' => $action,
-            'params' => $params
-        ];
+        return $match;
+    }
+
+    private function preparePattern($pattern, $requirements)
+    {
+        foreach ($requirements as $key => $val) {
+            if ($key === '_method') continue;
+            $pattern = str_replace('{' . $key . '}', '('.$val.')', $pattern);
+        }
+        return '/^' . str_replace('/', '\/', $pattern) . '$/';
+    }
+
+    private function getParameters($matchRoute, $uri)
+    {
+        if (! $matchRoute['requirements']) return [];
+        $matches = [];
+        preg_match($this->preparePattern($matchRoute['pattern'], $matchRoute['requirements']), $uri, $matches);
+
+        return array_slice($matches, 1);
     }
 }
