d9ef0e1044a80cdd8de43f28d9e563afad399b23
lion.w3x <lion.w3x@gmail.com>
2017-02-12 17:30:52 +0200

    Update entry point

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once(__DIR__ . '/../app/Loader.php');
+require_once(__DIR__ . '/app/Loader.php');
 
-$config = require_once(__DIR__ . '/../config/config.php');
+$config = require_once(__DIR__ . '/config/config.php');
 
-$app = new \App\Application($config);
+$app = new App\Application($config);
 
 $app->run();
